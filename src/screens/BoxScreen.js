683febd535f20fc8b17d28bf1805f6bb884c8bbe
Thanh Nguyen <thanhcnguyen@kms-technology.com>
2019-11-01 10:54:07 +0700

    new change

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,10 +4,9 @@ import {Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Child #1</Text>
-            <Text style={styles.textStyle}>Child #2</Text>
-            <Text style={styles.textStyle}>Child #3</Text>
-            <Text style={styles.textStyle}>Child #4</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
         </View>
     );
 }
@@ -16,15 +15,33 @@ const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
         borderColor: 'black',
-        height: 200,
+        height: 250,
         flexDirection: 'row',
-        justifyContent: "flex-end",
-        alignItems: "center"
+        justifyContent: "space-between",
+        alignItems: "flex-start"
     },
-    textStyle: {
+    textOneStyle: {
         borderWidth: 1,
-        borderColor: 'red'
+        borderColor: 'red',
+        height: 100,
+        width: 100
+    },
+    textTwoStyle: {
+        borderWidth: 1,
+        borderColor: 'red',
+        height: 100,
+        width: 100,
+        //position: 'absolute',
+        alignSelf: "flex-end",
+        // left: 10    
+    },
+    textThreeStyle: {
+        borderWidth: 1,
+        borderColor: 'red',
+        height: 100,
+        width: 100
     }
+
 });
 
 export default BoxScreen;
